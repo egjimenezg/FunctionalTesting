0dfd09d61cc815cfd6a9d614816a3f238b97d66c
egjimenezg <egjimenezg@gmail.com>
2015-08-15 20:53:29 -0500

    coding #3 adding functional test integrating Spock

---
+++ src/test/groovy/com/makingdevs/MakingDevsSiteFunctionalSpec.groovy
@@ -0,0 +1,36 @@
+package com.makingdevs
+
+import geb.spock.GebReportingSpec
+import java.lang.Void as Should
+import com.makingdevs.pages.*
+
+class MakingDevsSiteFunctionalSpec extends GebReportingSpec{
+
+  Should "show the available courses"(){
+    when:
+      to MakingDevsHomePage
+    and:
+      coursesButton.click()
+
+    then:
+      at MakingDevsCoursesPage
+  }
+
+  Should "show the course information"(){
+    when:
+      to MakingDevsCoursesPage
+    and:
+      groovyCoursesDiv.find("a[href^='${_selectorUrl}").click()
+
+    then:
+      at GroovyTestingPage
+      postTitle.text() == _postTitle
+    where:
+    _selectorUrl                    | _postTitle
+    "/training/groovy-testing"      | "Pruebas en la JVM con Groovy"
+    "/training/groovy-essentials"   | "El lenguaje dinámico Groovy"
+    "/training/grails"              | "Desarrollo de aplicaciones con Grails"
+    "/training/groovy-intermediate" | "Productividad con Groovy"
+  }
+
+}
